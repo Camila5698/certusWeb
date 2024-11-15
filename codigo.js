865cc875125d15a80fd1a501d5074a2e438d8990
Camila <marinacoaacosta16@gmail.com>
2024-11-14 20:59:09 -0500

    mi primer commit

---
+++ codigo.js
@@ -0,0 +1,27 @@
+/*Tipos de dato principales String (cadena de texto)
+String (Cadena de texto)
+Number
+Boolean*/
+
+/*Tipos de dato especiales (cadena de texto)
+Undefind
+Null (vacio)
+NaN (No hay numero - cuando pongo una letra en vez de numero)*/
+
+/*Para crear variables constantes
+Let
+Var
+Const*/
+
+/* Mi primera variable*/
+
+
+
+
+let numero = 0;
+
+do{
+    numero++;
+    document.write(numero + "<br>");
+}
+while(numero > 6)
