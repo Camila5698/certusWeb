f9392d826f05f7e6ab26bedd8113ed099ffcac88
Camila5698 <marinacoaacosta16@gmail.com>
2024-11-17 13:46:38 -0500

    sexto

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let i = 0;
+
 let colornormal = true;
-const slider1 = document.querySelector("#sld1");
-const slider2 = document.querySelector("#sld2");
-let resultadoSuma = 0
+
 
 const reactTemas = document.getElementsByClassName('ract-tema');
 const reactContainer = document.getElementById('react-xpand1');
@@ -10,45 +8,45 @@ const boton = document.getElementById('botonColor');
 const resultadoParrafo = document.getElementById('resultado');
 const items = document.querySelectorAll('.tachado');
 
-function mostrarClic(){
-    i+=1;
-    console.log(`clic ${i}`);
+
+
+
+let slider1 = document.getElementById('sld1');
+let slider2 = document.getElementById('sld2');
+let slider3 = document.getElementById('sld3');
+let slider4 = document.getElementById('sld4');
+let slider5 = document.getElementById('sld5');
+let value1 = document.getElementById('value1');
+let value2 = document.getElementById('value2');
+let value3 = document.getElementById('value3');
+let value4 = document.getElementById('value4');
+let value5 = document.getElementById('value5');
+let resultado = document.getElementById('fanLevel');
+
+
+function mostrarValores() {
+    value1.textContent = slider1.value;
+    value2.textContent = slider2.value;
+    value3.textContent = slider3.value;
+    value4.textContent = slider4.value;
+    value5.textContent = slider5.value;
+    
 }
 
-function mostrarValor(){
 
-    resultadoSuma = parseInt(slider1.value) + parseInt(slider2.value);    
-    console.log(resultadoSuma);
+function mostrarResultado() {
+   
+    mostrarValores();
+
+    
+    let resultadoSuma = parseInt(slider1.value) + parseInt(slider2.value) + parseInt(slider3.value) + parseInt(slider4.value) + parseInt(slider5.value);
+
+    
+    resultado.textContent = resultadoSuma;
+    console.log(`Nivel de fanatismo calculado: ${resultadoSuma}`);
 }
 
-function mostrarContent(objeto){
-    console.log(objeto.target.innerText);
-}
-
-
-
-
-
-function cambiarColor(){
-    if(colornormal){
-        reactContainer.style.backgroundColor = `#06586C`;
-        resultadoParrafo.style.color = `#06586C`;
-    }
-    else{
-        reactContainer.style.backgroundColor = `#e6e6e6`;
-        resultadoParrafo.textContent = resultadoSuma;
-    }
-    colornormal = !colornormal;
-
-}
-
-//aqui arriba puedo porer texto oculto
-
-for (const tema of reactTemas){
-    tema.addEventListener('click', mostrarContent)
-}
-
-boton.addEventListener('click', cambiarColor)
+mostrarValores();
 
 
 
@@ -56,11 +54,53 @@ boton.addEventListener('click', cambiarColor)
 
 
 
-// tachado
 
-items.forEach(item => {
+
+
+
+
+
+
+
+
+
+
+document.querySelectorAll('.tachado').forEach(item => {
     item.addEventListener('click', function() {
-      
-      item.classList.toggle('tachado-activo');
+       
+        item.classList.toggle('tachado-activo');
+
+       
+        if (item.classList.contains('tachado-activo')) {
+            
+            if (item.id === 'uno') {
+                item.classList.add('verde');  
+                item.classList.remove('rojo');
+            } else if (item.id === 'dos') {
+                item.classList.add('rojo');  
+                item.classList.remove('verde');
+            } else if (item.id === 'cuatro' || item.id === 'cinco') {
+                item.classList.add('verde');  
+                item.classList.remove('rojo');
+            } else {
+                item.classList.add('rojo'); 
+                item.classList.remove('verde');
+            }
+        } else {
+            
+            if (item.id === 'uno') {
+                item.classList.add('black');  
+                item.classList.remove('verde');
+            } else if (item.id === 'dos') {
+                item.classList.add('black');  
+                item.classList.remove('verde');
+            } else if (item.id === 'cuatro' || item.id === 'cinco') {
+                item.classList.add('black');  
+                item.classList.remove('rojo');
+            } else {
+                item.classList.add('black');  
+                item.classList.remove('verde');
+            }
+        }
     });
-})
+});
